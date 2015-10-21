4555ece3a4bb60a71e5ed3ab2e131b22a9d679b0
tylerb <tylerb@dmcinfo.com>
2015-10-20 18:07:33 -0600

    Working version. Need player order

--- app/src/main/java/com/example/android/javatesting/MainActivity.java
+++ app/src/main/java/com/example/android/javatesting/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends ActionBarActivity {
         Log.d(TAG, "Ball ID is:  " + BallID);
         switch (BallID) {
             case "b1":
+            case "b3_1":
                 if (ball.isActivated()){
                     //Log.d(TAG, "Toggle method called in activated");
                     ball.setBackgroundResource(R.drawable.one);
@@ -96,7 +97,9 @@ public class MainActivity extends ActionBarActivity {
                     ball.setBackgroundResource(R.drawable.one_out);
                     ball.setActivated(true);
                 }
+                break;
             case "b2":
+            case "b3_2":
                 if (ball.isActivated()){
                     ball.setBackgroundResource(R.drawable.two);
                     ball.setActivated(false);
@@ -104,7 +107,137 @@ public class MainActivity extends ActionBarActivity {
                 else{
                     ball.setBackgroundResource(R.drawable.two_out);
                     ball.setActivated(true);
+                }break;
+            case "b3":
+            case "b3_3":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.three);
+                    ball.setActivated(false);
                 }
+                else{
+                    ball.setBackgroundResource(R.drawable.three_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_4":
+            case "b4":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.four);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.four_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_5":
+            case "b5":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.five);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.five_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_6":
+            case "b6":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.six);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.six_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_7":
+            case "b7":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.seven);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.seven_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_8":
+            case "b8":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.eight);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.eight_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_9":
+            case "b9":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.nine);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.nine_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_10":
+            case "b10":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.ten);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.ten_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_11":
+            case "b11":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.eleven);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.eleven_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_12":
+            case "b12":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.twelve);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.twelve_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_13":
+            case "b13":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.thirteen);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.thirteen_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_14":
+            case "b14":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.fourteen);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.fourteen_out);
+                    ball.setActivated(true);
+                }break;
+            case "b3_15":
+            case "b15":
+                if (ball.isActivated()){
+                    ball.setBackgroundResource(R.drawable.fifteen);
+                    ball.setActivated(false);
+                }
+                else{
+                    ball.setBackgroundResource(R.drawable.fifteen_out);
+                    ball.setActivated(true);
+                }break;
         }
     }
 
